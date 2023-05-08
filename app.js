53c8789fa4e77d1e79cb8795d8795fe8ad32f2f2
Shubham Abnave <abnaveshubham.se@gmail.com>
2023-05-08 09:07:19 +0530

    parent child sibling html tags using react

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
-const heading = React.createElement("h1", {id : "heading"}, "Hello World from React!");
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  [React.createElement(
+    "div",
+    { id: "child1" },
+    [React.createElement("h1", {}, "I am H1 Tag"),
+    React.createElement("h2", {}, "I am H2 Tag")]
+  ),
+  React.createElement(
+    "div",
+    { id: "child2" },
+    [React.createElement("h1", {}, "I am H1 Tag"),
+    React.createElement("h2", {}, "I am H2 Tag")]
+  )]
+);
+//const heading = React.createElement("h1", {id : "heading"}, "Hello World from React!");
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);  
+root.render(parent);
