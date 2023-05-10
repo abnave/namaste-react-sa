add83d00c67b70b5eb6cbffe210e21a3d83fb613
Shubham Abnave <abnaveshubham.se@gmail.com>
2023-05-10 08:22:37 +0530

    episode-02 prod build

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 const parent = React.createElement(
   "div",
   { id: "parent" },
@@ -17,3 +19,4 @@ const parent = React.createElement(
 //const heading = React.createElement("h1", {id : "heading"}, "Hello World from React!");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
+//  "main": "app.js",
