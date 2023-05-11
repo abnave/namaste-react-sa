d019ec404967518b5e25fbaaacde97f6a9bf7b51
Shubham Abnave <abnaveshubham.se@gmail.com>
2023-05-11 21:51:22 +0530

    episode-03 playing with JSX

--- app.js
+++ app.js
@@ -1,11 +1,44 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const jsxHeading = (
-  <h1 id="heading-1" className="heading">
-    Hello World, using JSX
-  </h1>
+
+const info = (<h1>I will add some information here</h1>)
+const FirstComponent = () => {
+  return <h1>I am first component with return keyword</h1>;
+};
+const SecondComponent = () => (
+  <React.Fragment>
+  <div>
+    {info}
+    <FirstComponent />
+    <h1> I am second component without using return</h1>
+  </div>
+  <div>
+    {info}
+    <FirstComponent />
+    <h1> I am repeated second component without using return</h1>
+  </div>
+  </React.Fragment>
 );
+const ThirdComponent = () => (
+  <>
+  <div>
+    {info}
+    <FirstComponent />
+    <h1> I am third component without using return</h1>
+  </div>
+  <div>
+    {info}
+    <FirstComponent />
+    <h1> I am repeated third component without using return</h1>
+  </div>
+  </>
+);
+const info2 = (<div>
+  <FirstComponent />
+  <SecondComponent />
+  <ThirdComponent />
+</div>);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading);
+root.render(info2);
 //  "main": "app.js",
