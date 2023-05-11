663728f2bdbb2c397812f1d2d83fcdb17c9bdb43
Shubham Abnave <abnaveshubham.se@gmail.com>
2023-05-11 17:26:41 +0530

    episode-03 React code using JSX

--- app.js
+++ app.js
@@ -1,22 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  [React.createElement(
-    "div",
-    { id: "child1" },
-    [React.createElement("h1", {}, "I am H1 Tag"),
-    React.createElement("h2", {}, "I am H2 Tag")]
-  ),
-  React.createElement(
-    "div",
-    { id: "child2" },
-    [React.createElement("h1", {}, "I am H1 Tag"),
-    React.createElement("h2", {}, "I am H2 Tag")]
-  )]
+
+const jsxHeading = (
+  <h1 id="heading-1" className="heading">
+    Hello World, using JSX
+  </h1>
 );
-//const heading = React.createElement("h1", {id : "heading"}, "Hello World from React!");
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(jsxHeading);
 //  "main": "app.js",
