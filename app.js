328afe9c0e609f96139c9dc557b9949b484fb291
Shubham Abnave <abnaveshubham.se@gmail.com>
2023-05-13 16:03:09 +0530

    just playing around

--- app.js
+++ app.js
@@ -4,19 +4,19 @@ import ReactDOM from "react-dom/client";
 
 const info = (<h1>I will add some information here</h1>)
 const FirstComponent = () => {
-  return <h1>I am first component with return keyword</h1>;
+  return <h1>First Component</h1>;
 };
 const SecondComponent = () => (
   <React.Fragment>
   <div>
     {info}
     <FirstComponent />
-    <h1> I am second component without using return</h1>
+    <h2>second component</h2>
   </div>
   <div>
     {info}
     <FirstComponent />
-    <h1> I am repeated second component without using return</h1>
+    <h2> I am repeated second component without using return</h2>
   </div>
   </React.Fragment>
 );
@@ -25,17 +25,17 @@ const ThirdComponent = () => (
   <div>
     {info}
     <FirstComponent />
-    <h1> I am third component without using return</h1>
+    <h3> I am third component without using return</h3>
   </div>
   <div>
     {info}
     <FirstComponent />
-    <h1> I am repeated third component without using return</h1>
+    <h3> I am repeated third component without using return</h3>
   </div>
   </>
 );
 const info2 = (<div>
-  <FirstComponent />
+  {FirstComponent}
   <SecondComponent />
   <ThirdComponent />
 </div>);
